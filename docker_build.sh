2191fffdac232034c5d7720621b53d6db8beec36
Rajiv Gangadharan <rajiv.gangadharan@gmail.com>
2021-08-27 16:46:22 +0530

    base commit for inflow-outflow issue reporting

---
+++ docker_build.sh
@@ -0,0 +1,1 @@
+docker build . -t rajivgangadharan/mtrxbook  --no-cache
---
+++ docker_run_debug.sh
@@ -0,0 +1,7 @@
+# Converts all the tsv files in a directory to rds file
+# Rajiv Gangadharan 11/Aug/2021
+#
+user_name=`id -un`
+data_dir=data
+docker rm portfolio-insights
+docker run --name portfolio-insights -v/var/www/html:/usr/src/www/html -v/home/rajivg/Documents/Work/Code/Datasets:/usr/src/inst/extdata rajivgangadharan/mtrxbook
