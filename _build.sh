ea73a1357debd1ed0345451d0fadd1542ef2f181
Rajiv Gangadharan <rajiv.gangadharan@gmail.com>
2022-02-19 10:24:56 +0530

    Removing build for pdf and epub formats

--- _build.sh
+++ _build.sh
@@ -11,6 +11,6 @@ Rscript -e 'webshot::install_phantomjs(force=TRUE)'
 
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
